0a952468502ea3ab9b62a7b0f113222cf2183351
teacat <ubscoder@gmail.com>
2021-09-04 17:21:49 +0300

    vee validate rules

--- src/components/AuthModal.vue
+++ src/components/AuthModal.vue
@@ -87,11 +87,12 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <form v-if="tab === 'register'">
+          <VeeForm v-if="tab === 'register'">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
-              <input
+              <VeeField
+                  name="name"
                   type="text"
                   class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
@@ -164,7 +165,7 @@
             >
               Submit
             </button>
-          </form>
+          </VeeForm>
         </div>
       </div>
     </div>
